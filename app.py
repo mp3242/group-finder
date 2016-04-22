e621a5342f4d8127a235496c772dbf63dcda76cb
Hidy <yh2635@columbia.edu>
2016-04-22 13:36:21 -0400

    login setup

--- app.py
+++ app.py
@@ -27,13 +27,13 @@ def load_user(id):
 @app.route('/login',methods=['GET','POST'])
 @oid.loginhandler
 def login():
-    if g.user is not None and g.user.is_authenticated:
-        return redirect(url_for('index'))
-    form=LoginForm()
-    if form.validate_on_submit():
-        session['remember me']=form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['email','password'])
-    return render_template('login.html',title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])
+    #if g.user is not None and g.user.is_authenticated:
+    #    return redirect(url_for('index'))
+    #form=LoginForm()
+    #if form.validate_on_submit():
+    #    session['remember me']=form.remember_me.data
+    #    return oid.try_login(form.openid.data, ask_for=['email','password'])
+    return render_template('login.html')
 
 @oid.after_login
 def after_login(resp):
